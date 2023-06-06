86c3d1d59d573abfd54e39dc56e94f9ddd28a4f3
Aakash2510Jain <aakash.jain@utilitarianlab.com>
2023-06-06 21:05:56 +0530

    6/6/2023

--- force-app/main/default/aura/Create_And_Show_Leads_On_Site_Visit/Create_And_Show_Leads_On_Site_VisitController.js
+++ force-app/main/default/aura/Create_And_Show_Leads_On_Site_Visit/Create_And_Show_Leads_On_Site_VisitController.js
@@ -1,28 +1,24 @@
 ({
     doInit  : function(component, event, helper){
         debugger;
-      component.set("v.showRecordDetail",false);  
-      //  helper.fetchvisitrec(component, event);
+        component.set("v.showRecordDetail",false);  
+        //  helper.fetchvisitrec(component, event);
     },
-	searchLead : function(component, event, helper) {
+    searchLead : function(component, event, helper) {
         debugger;
-         
-          var controllingFieldAPI = component.get("v.controllingFieldAPI");
+        
+        var controllingFieldAPI = component.get("v.controllingFieldAPI");
         var dependingFieldAPI = component.get("v.dependingFieldAPI");
         var subDependingFieldAPI = component.get("v.subDependingFieldAPI");
-        
         var objDetails = component.get("v.objDetail");
-        // call the helper function
-        helper.fetchPicklistValues(component,objDetails,controllingFieldAPI, dependingFieldAPI, "v.depnedentFieldMap");
         
-        // 2nd and 3ed picklist 
+        helper.fetchPicklistValues(component,objDetails,controllingFieldAPI, dependingFieldAPI, "v.depnedentFieldMap");
         helper.fetchPicklistValues(component,objDetails,dependingFieldAPI, subDependingFieldAPI, "v.subDepnedentFieldMap");
-       
-   
-    component.set("v.showVisitButton",true); 
-         component.set("v.showNewLeadPage",false); 
-       var action =component.get("c.showExistingLead");
-       var searchEmailOrPhone = component.get("v.Searchkey");
+        
+        component.set("v.showVisitButton",true); 
+        component.set("v.showNewLeadPage",false); 
+        var action =component.get("c.showExistingLead");
+        var searchEmailOrPhone = component.get("v.Searchkey");
         
         action.setParams({
             "searchkey" : searchEmailOrPhone
@@ -30,47 +26,44 @@
         action.setCallback(this,function(response){
             var state = response.getState();
             if(state === "SUCCESS"){
-                  
+                
                 var data = response.getReturnValue();
                 
                 if(data != null){
                     component.set("v.LeadRec",data);
-                  component.set("v.leadRecId",data.Id); 
-                     helper.fetchvisitrec(component, event);
+                    component.set("v.leadRecId",data.Id); 
+                    helper.fetchvisitrec(component, event);
                     component.set("v.showRecordDetail",true); 
                     component.set("v.showError",false);
                 }else{
-                      component.set("v.showRecordDetail",false);
-                     component.set("v.showError",true);
-                     component.set("v.showVisitButton",false); 
-                  
-                     
+                    component.set("v.showRecordDetail",false);
+                    component.set("v.showError",true);
+                    component.set("v.showVisitButton",false); 
                 } 
             }
         });
-         $A.enqueueAction(action);
-	},
+        $A.enqueueAction(action);
+    },
     createvisit : function(component, event, helper){
         debugger;
-         component.set("v.showVisitPage",true); 
-         component.set("v.showRecordDetail",false);
-         component.set("v.showNewLeadPage",false); 
-           var today = new Date().toISOString().split('T')[0];
+        component.set("v.showVisitPage",true); 
+        component.set("v.showRecordDetail",false);
+        component.set("v.showNewLeadPage",false); 
+        var today = new Date().toISOString().split('T')[0];
         component.set('v.today', today);
-         
+        
     },
     selectdate : function(component, event, helper){
         debugger;
         var dateselect = component.find('date').get('v.value');
-         var todaydate = new Date().toISOString().split('T')[0];
+        var todaydate = new Date().toISOString().split('T')[0];
         if(dateselect < todaydate){
-             alert('Date should Be Gretar than Today Date');
-             component.set("v.today",todaydate); 
+            alert('Date should Be Gretar than Today Date');
+            component.set("v.today",todaydate); 
         }else{
-              
             component.set("v.today",dateselect);  
-         }
-       
+        }
+        
     },
     createVistRec : function(component, event, helper){
         debugger;
@@ -79,74 +72,67 @@
         var visitRec = component.get("v.SiteVistRec");
         var changdate = component.get("v.today");
         
-         action.setParams({
+        action.setParams({
             "recid" : leadid,
-           "changedate"  : changdate,
-              "newVisitRec" : visitRec
+            "changedate"  : changdate,
+            "newVisitRec" : visitRec
         });
         action.setCallback(this,function(response){
             var state = response.getState();
             if(state === "SUCCESS"){
-                 alert('New  Visit has been Created under Current Lead');
+                alert('New  Visit has been Created under Current Lead');
                 component.set("v.showRecordDetail",true);
-                 component.set("v.showVisitPage", false);
+                component.set("v.showVisitPage", false);
             }
         });
-         $A.enqueueAction(action);
+        $A.enqueueAction(action);
         
     }, 
-     closeModal: function(component, event, helper) {
+    closeModal: function(component, event, helper) {
         component.set("v.showVisitPage", false);
-          component.set("v.showRecordDetail",true);
+        component.set("v.showRecordDetail",true);
     },
     
     UpdateLead :function(component, event, helper){
         debugger;
-      var action =component.get("c.updateExistinglead");  
+        var action =component.get("c.updateExistinglead");  
         var Leadrecid = component.get("v.leadRecId"); 
-         var leadRecords = component.get("v.LeadRec"); 
-       action.setParams({
+        var leadRecords = component.get("v.LeadRec"); 
+        action.setParams({
             "recid" : Leadrecid,
-           "leadRec" : leadRecords
+            "leadRec" : leadRecords
         });
         action.setCallback(this,function(response){
             var state = response.getState();
             if(state === "SUCCESS"){
-                 alert('Record get Updated'); 
+                alert('Record get Updated'); 
             }
         });
-         $A.enqueueAction(action); 
+        $A.enqueueAction(action); 
         
     },
     CreateLead:function(component, event, helper){
         debugger;
         component.set("v.showVisitPage",false);
-         component.set("v.showNewLeadPage",true); 
-         component.set("v.showRecordDetail",false); 
-         
+        component.set("v.showNewLeadPage",true); 
+        component.set("v.showRecordDetail",false); 
+        
         var controllingFieldAPI = component.get("v.controllingFieldAPI");
         var dependingFieldAPI = component.get("v.dependingFieldAPI");
         var subDependingFieldAPI = component.get("v.subDependingFieldAPI");
         
         var objDetails = component.get("v.objDetail");
-        // call the helper function
         helper.fetchPicklistValues(component,objDetails,controllingFieldAPI, dependingFieldAPI, "v.depnedentFieldMap");
-        
-        // 2nd and 3ed picklist 
         helper.fetchPicklistValues(component,objDetails,dependingFieldAPI, subDependingFieldAPI, "v.subDepnedentFieldMap");
         
     },
     onControllerFieldChange: function(component, event, helper) {
         debugger;
-        var controllerValueKey = event.getSource().get("v.value"); // get selected controller field value
+        var controllerValueKey = event.getSource().get("v.value"); 
         var depnedentFieldMap = component.get("v.depnedentFieldMap");
         
         if (controllerValueKey != '--- None ---') {
-            // disable and reset sub dependent field 
-           
-           
             var ListOfDependentFields = depnedentFieldMap[controllerValueKey];
-            
             if(ListOfDependentFields.length > 0){
                 component.set("v.bDisabledDependentFld" , false);  
                 helper.fetchDepValues(component, ListOfDependentFields,"v.listDependingValues");    
@@ -169,7 +155,7 @@
         debugger;
         var controllerValueKey = event.getSource().get("v.value"); // get selected sub controller field value
         var depnedentFieldMap = component.get("v.subDepnedentFieldMap");
-       
+        
         if (controllerValueKey != '--- None ---') {
             var ListOfDependentFields = depnedentFieldMap[controllerValueKey];
             if(ListOfDependentFields.length > 0){
@@ -187,19 +173,19 @@
     },
     createNEwLead :function(component, event, helper){
         debugger;
-       var action =component.get("c.createNewLead");  
-          var newleadRecords = component.get("v.objDetail"); 
-         action.setParams({
-           
-           "createleadRec" : newleadRecords
+        var action =component.get("c.createNewLead");  
+        var newleadRecords = component.get("v.objDetail"); 
+        action.setParams({
+            
+            "createleadRec" : newleadRecords
         });
         action.setCallback(this,function(response){
             var state = response.getState();
             if(state === "SUCCESS"){
-                 alert('New Lead has been Created'); 
-                 component.set("v.showNewLeadPage",false); 
+                alert('New Lead has been Created'); 
+                component.set("v.showNewLeadPage",false); 
             }
         });
-         $A.enqueueAction(action);  
+        $A.enqueueAction(action);  
     }
 })
